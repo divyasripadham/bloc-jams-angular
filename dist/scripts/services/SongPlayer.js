e4de312381722e65ee1441e91a5beeeed6d2aaea
divyasripadham <divya_sri16@yahoo.com>
2016-04-18 22:37:42 -0400

    Assignment 7 Services 2:completed

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -30,11 +30,21 @@
             currentSong = song;
         };
 
+        /**
+       * @function playSong
+       * @desc Calls play on currentBuzzObject and sets the playing attribute of song to true
+       * @param {Object} song
+       */
+
+        var playSong = function(song){
+          currentBuzzObject.play();
+          song.playing = true;
+        };
+
         SongPlayer.play = function(song){
           if(currentSong !== song){
               setSong(song);
-              currentBuzzObject.play();
-              song.playing = true;
+              playSong(song);
           } else if(currentSong === song){
               if(currentBuzzObject.isPaused()){
                   currentBuzzObject.play();
