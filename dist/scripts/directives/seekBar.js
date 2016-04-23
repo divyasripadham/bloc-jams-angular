d996f96f9d48c816b0399dd8963a062d26fbd362
divyasripadham <divya_sri16@yahoo.com>
2016-04-22 23:01:03 -0400

    Assignment 9 Directives 1 completed, forgot to create assignment branch

--- dist/scripts/directives/seekBar.js
+++ dist/scripts/directives/seekBar.js
@@ -30,6 +30,10 @@
                    return {width: percentString()};
                };
 
+               scope.thumbStyle = function() {
+                   return {left: percentString()};
+               };
+
                scope.onClickSeekBar = function(event){
                    var percent = calculatePercent(seekBar, event);
                    scope.value = percent * scope.max;
@@ -38,9 +42,9 @@
                scope.trackThumb = function() {
                    $document.bind('mousemove.thumb', function(event) {
                        var percent = calculatePercent(seekBar, event);
-                       scope.$apply(function() {
+                      //  scope.$apply(function() {
                            scope.value = percent * scope.max;
-                       });
+                      //  });
                    });
 
                    $document.bind('mouseup.thumb', function() {
